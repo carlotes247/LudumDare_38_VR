ce2192c93e3d86f6cf80f85f01a2023129536d3a
PC-DCLabs <cgd506@york.ac.uk>
2017-05-13 01:40:34 +0100

    Version 1.3 finished: - Grab button changed to Touchpad - Collider of controllers made bigger for better object detection (controller object doubled in size, model object reduced in half) - Chest now have textures - Food options now have a food model - Food options will respawn in original location when staying in the water for 2 seconds - Rigibody of playArea made to kinematic with all constraints activated to avoid it jumping - Reworked colliders of food prefabs - Made food bigger (again)

---
+++ Assets/Carlos/Scripts/OnTriggerStayCountdown.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerStayCountdown : MonoBehaviour {
+
+    // Timer to count seconds
+    private TimerController m_Timer;
+
+    // the amount of seconds to count
+    public float SecondsToCount;
+
+    // The tag of the trigger
+    public string TagToCompare;
+
+    // Position to respawn
+    public Transform m_PosToRespawn;
+
+	// Use this for initialization
+	void Start () {
+        //m_PosToRespawn = this.transform.position;
+        m_Timer = this.gameObject.AddComponent<TimerController>();
+        m_Timer.ObjectLabel = "Respawn_Timer";
+	}
+
+    // Ontriggerstay we run the countdown
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag(TagToCompare))
+        {
+            if (m_Timer.GenericCountDown(SecondsToCount))
+            {
+                // When countdown finished, we respawn the object
+                RespawnObject();
+            }
+        }
+            
+    }
+
+    // OnTriggerExit we stop the countdown
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(TagToCompare))
+        {
+            m_Timer.StopTimer();
+        }
+    }
+
+    // Respawns the object somewhere
+    private void RespawnObject()
+    {
+        this.transform.position = m_PosToRespawn.position;
+    }
+}
